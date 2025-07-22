8d2d3c10531bd1b58f8bad13698f0cb5ebccf100
Ayush Rajpal <ayushrajpal2021@gmail.com>
2025-07-22 10:41:42 +0530

    setup

---
+++ Apple_Framework/Apple_FrameworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworkApp.swift
+//  Apple_Framework
+//
+//  Created by Ayush Rajpal on 22/07/25.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apple_Framework/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Apple_Framework
+//
+//  Created by Ayush Rajpal on 22/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
